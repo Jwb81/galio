2c94b234327b2f684f1ae47ba36302ecb5d33a6a
Jason Barnett <jayb21@neo.rr.com>
2020-02-09 12:08:02 -0500

    update screen wrapper to get padding internally

--- src/components/ScreenWrapper/ScreenWrapper.js
+++ src/components/ScreenWrapper/ScreenWrapper.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '../../theme';
+import Block from '../Block';
+import { Platform, StatusBar } from 'react-native';
 
-function ScreenWrapper({ children, ...rest }) {
-  const wrapperStyles = [styles.background, styles.screen, style];
+function ScreenWrapper({ children, styles, style, ...rest }) {
+  const wrapperStyles = [
+    styles.background,
+    {
+      paddingTop: Platform.select({
+        ios: 0,
+        android: StatusBar.currentHeight,
+      }),
+    },
+    styles.screenWrapper,
+    style,
+  ];
 
   return (
     <Block flex safe {...rest} style={wrapperStyles}>
